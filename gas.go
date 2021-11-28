ca099724d8b7d8e55ea4a685bccc34b97c2e1e2f
davisgriffin <70169481+davisgriffin@users.noreply.github.com>
2021-11-28 12:40:34 -0500

    changed for config setup

--- gas.go
+++ gas.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"log"
 	"math"
 	"math/big"
 	"math/rand"
 	"time"
 
 	mathEth "github.com/ethereum/go-ethereum/common/math"
+	"github.com/griffindavis02/eth-bit-flip/config"
 	"github.com/griffindavis02/eth-bit-flip/injection"
 )
 
@@ -24,30 +28,57 @@ var (
 	mNonce           uint = 0
 	mGasPrice        uint = 21000
 	mTransactionTrie []Transaction
+	// intTest          int    = 1
+	// intTest32        int32  = 2
+	// intTest64        int64  = 3
+	// uintTest         uint   = 4
+	// uintTest32       uint32 = 5
+	// uintTest64       uint64 = 6
 )
 
 func main() {
 
-	// Initialize testing environment with error rates and output
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+	bytes, err := json.MarshalIndent(cfg, "", "    ")
+	if err == nil {
+		fmt.Println(string(bytes))
+	}
 
 	// Set up transaction
-	for range make([]int, 10000) {
-		wei, _ := toWei(0.05, "ether")
+	for range make([]int, 20) {
+		// intTest = injection.BitFlip(intTest, &cfg).(int)
+		// // time.Sleep(time.Millisecond)
+		// intTest32 = injection.BitFlip(intTest32, &cfg).(int32)
+		// // time.Sleep(time.Millisecond)
+		// intTest64 = injection.BitFlip(intTest64, &cfg).(int64)
+		// // time.Sleep(time.Millisecond)
+		// uintTest = injection.BitFlip(uintTest, &cfg).(uint)
+		// // time.Sleep(time.Millisecond)
+		// uintTest32 = injection.BitFlip(uintTest32, &cfg).(uint32)
+		// // time.Sleep(time.Millisecond)
+		// uintTest64 = injection.BitFlip(uintTest64, &cfg).(uint64)
+		// time.Sleep(time.Millisecond)
+		wei, _ := toWei(0.05, "ether", &cfg)
 		sendTransaction("0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8",
 			"0xac03bb73b6a9e108530aff4df5077c2b3d481e5a",
 			wei,
-			22000)
+			22000,
+			&cfg)
 	}
 	// mjsonOut.PostAPI("http://localhost:5000/express")
 }
 
-func sendTransaction(from string, to string, value *big.Int, gasCap uint) string {
+func sendTransaction(from string, to string, value *big.Int, gasCap uint, cfg *config.Config) string {
 
 	gasCap = uint(mathEth.U256(big.NewInt(int64(gasCap))).Uint64())
 
-	fAddress := injection.BitFlip(from[2:], `E:\Libraries\Documents\Projects\Code\Capstone\go-ethereum\cmd\flipconfig\flipconfig.json`).(string)
-	tAddress := injection.BitFlip(to[2:], `E:\Libraries\Documents\Projects\Code\Capstone\go-ethereum\cmd\flipconfig\flipconfig.json`).(string)
-	value = injection.BitFlip(value, `E:\Libraries\Documents\Projects\Code\Capstone\go-ethereum\cmd\flipconfig\flipconfig.json`).(*big.Int)
+	fAddress := injection.BitFlip(from[2:], cfg).(string)
+	tAddress := injection.BitFlip(to[2:], cfg).(string)
+	value = injection.BitFlip(value, cfg).(*big.Int)
+	// fmt.Printf("%s\n%s\n%v\n\n", fAddress, tAddress, value)
 
 	hash := big.NewInt(0).
 		Rand(rand.New(rand.NewSource(time.Now().UnixNano())),
@@ -64,11 +95,11 @@ func sendTransaction(from string, to string, value *big.Int, gasCap uint) string
 		mNonce++
 	}
 
-	hash = injection.BitFlip(hash, `E:\Libraries\Documents\Projects\Code\Capstone\go-ethereum\cmd\flipconfig\flipconfig.json`).(*big.Int)
+	hash = injection.BitFlip(hash, cfg).(*big.Int)
 	return "0x" + hex.EncodeToString((hash.Bytes()))
 }
 
-func toWei(value float64, factor string) (*big.Int, error) {
+func toWei(value float64, factor string, cfg *config.Config) (*big.Int, error) {
 	var multiplicand float64 = 0
 	for value < 1 {
 		multiplicand++
@@ -79,11 +110,11 @@ func toWei(value float64, factor string) (*big.Int, error) {
 	switch factor {
 	case "ether":
 		returnValue := big.NewInt(0).Mul(bigValue, big.NewInt(int64(math.Pow(10, 18-multiplicand))))
-		returnValue = injection.BitFlip(returnValue, `E:\Libraries\Documents\Projects\Code\Capstone\go-ethereum\cmd\flipconfig\flipconfig.json`).(*big.Int)
+		returnValue = injection.BitFlip(returnValue, cfg).(*big.Int)
 		return returnValue, nil
 	case "gwei":
 		returnValue := big.NewInt(0).Mul(bigValue, big.NewInt(int64(math.Pow(10, 18-multiplicand))))
-		returnValue = injection.BitFlip(returnValue, `E:\Libraries\Documents\Projects\Code\Capstone\go-ethereum\cmd\flipconfig\flipconfig.json`).(*big.Int)
+		returnValue = injection.BitFlip(returnValue, cfg).(*big.Int)
 		return returnValue, nil
 	}
 	return nil, errors.New("invalid_factor")
